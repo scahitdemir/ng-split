32731ec5ba2da2dea1d04812f7ecf3acbfabc704
S. Cahit Demir <scahit.demir@embonds.com>
2016-12-02 18:31:23 +0200

    Add all files into the repository

---
+++ Gruntfile.js
@@ -0,0 +1,98 @@
+module.exports = function (grunt) {
+  // * Read command-line switches
+  // - Read in --browsers CLI option; split it on commas into an array if it's a string, otherwise ignore it
+  var browsers = typeof grunt.option('browsers') == 'string' ? grunt.option('browsers').split(',') : undefined;
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    library: grunt.file.readJSON('bower.json'),
+    concat: {
+      options: {
+        separator: ''
+      },
+      dist: {
+        src: [
+          'src/*.js'
+        ],
+        dest: 'dist/<%= library.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      jid: {
+        files: {
+          'dist/<%= library.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    jshint: {
+      beforeConcat: {
+        src: ['gruntfile.js', '<%= library.name %>/**/*.js']
+      },
+      afterConcat: {
+        src: [
+          '<%= concat.dist.dest %>'
+        ]
+      },
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true,
+          angular: true
+        },
+        globalstrict: false,
+        newcap: false
+      }
+    },
+    testFiles: {
+      karmaUnit: 'karma.conf.js'
+    },
+    karma: {
+      unit: {
+        options: {
+          configFile: '<%= testFiles.karmaUnit %>',
+          autoWatch: false,
+          singleRun: true,
+          browsers: browsers || ['Chrome']
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      files: [
+        'Gruntfile.js',
+        'src/**/*'
+      ],
+      tasks: ['default']
+    },
+    connect: {
+      server: {
+        options: {
+          hostname: '*'
+        }
+      }
+    }
+  });
+
+  // Load grunt-karma task plugin
+  grunt.loadNpmTasks('grunt-karma');
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask('test', ['jshint', 'karma:unit']);
+  grunt.registerTask('default', ['jshint:beforeConcat', 'concat', 'jshint:afterConcat', 'uglify']);
+  grunt.registerTask('livereload', ['default', 'connect:server', 'watch']);
+
+};
+
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# test
+# Ng-Split
+
+This angular directive is wrapper for[Split.js](http://nathancahill.github.io/Split.js/).
+
+## Download Ng-Split
+
+You can install ng-split via npm and Bower and its dependencies will be downloaded
+automatically:
+
+#### Npm
+```bash
+$ npm install ng-split --save
+```
+
+#### Bower
+```bash
+$ bower install ng-split --save
+```
+
+## Inject the Ng-Split Directive
+- Add ng-split.min.js to your main file (index.html).
+
+- Set `ng-split` as a dependency in your module
+```javascript
+var myapp = angular.module('myapp', ['ng-split'])
+```
